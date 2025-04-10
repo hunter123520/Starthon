8d147341f52b55a4918dbf4dacfd9b36b245cde5
Sellami Mohammed Abdelhadi <philippe.fernandez.2002@gmail.com>
2025-04-10 12:35:10 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Sellami Mohammed Abdelhadi</title>
+      <title>CampGuide</title>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
